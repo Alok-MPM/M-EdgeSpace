ea0ed34918bd6cf4660fe77ca4f52d4e64e3909d
Alok-MPM <legendsigmamale@gmail.com>
2026-09-15 21:24:31 +0530

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const C='holo-v4';
+const C='holo-v6';
 self.addEventListener('install',e=>{self.skipWaiting();
- e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./fx.js','core.js'.replace('',''),'./core.js','./voiceai.js','./sw.js'])))});
+ e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./fx.js','./core.js','./voiceai.js','./studio3d.js','./sw.js'])))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{
  for(const k of await caches.keys())if(k!==C)await caches.delete(k);
  await clients.claim()})()));
